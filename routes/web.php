3a0e0c503fd23f0a17aafa1debd49e6e9f1c6ac5
Blankphrase <ckasera6486@gmail.com>
2018-11-21 17:34:47 +0300

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -19,45 +19,45 @@ $api = router('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
 
+    
+    $api->post('/login', 'AuthController@login');
+    $api->get('/logout', 'AuthController@logout');
+    $api->post('/register', 'AuthController@register');
+    
+    
+    
+    $api->get('/accounts', 'AccountController@index');
+    $api->get('/accounts/{account}', 'AccountController@show');
+    $api->post('/accounts', 'AccountController@store');
+    $api->put('/accounts/{accounts}','AccountController@update');
+    $api->delete('/accounts/{accounts}','AccountController@destroy');
+    
+    
+    $api->get('/accouts/{account}/contact','ContactController@index');
+    $api->post('/accouts/{account}/contact','ContactController@store');
+    $api->get('/accouts/{account}/contact/{contact}','ContactController@show');
+    $api->put('/accouts/{account}/contact/{contact}','ContactController@update');
+    $api->delete('/accouts/{account}/contact/{contact}','ContactController@destroy');
+    
+    
+    
+    $api->get('/accouts/{account}/contact/{contact}/opportunity','OpportunityController@index');
+    $api->post('/accouts/{account}/contact/{contact}/opportunity','OpportunityController@store');
+    $api->get('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@show');
+    $api->put('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@update');
+    $api->delete('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@destroy');
+    
+    
+    $api->get('/meetings', 'MeetingController@index');
+    $api->get('/meetings/{account}', 'MeetingController@show');
+    $api->post('/meetings', 'MeetingController@store');
+    $api->put('/meetings/{meetings}','MeetingController@update');
+    $api->delete('/meetings/{meetings}','MeetingController@destroy');
+    
+    
+    $api->get('/messages', 'MessageController@index');
+    $api->get('/messages/{message}', 'MessageController@show');
+    $api->post('/messages', 'MessageController@store');
+    $api->put('/messages/{messages}','MessageController@update');
+    $api->delete('/messages/{messages}','MessageController@destroy');
 });
-
-$router->post('/login', 'AuthController@login');
-$router->get('/logout', 'AuthController@logout');
-$router->post('/register', 'AuthController@register');
-
-
-
-$router->get('/accounts', 'AccountController@index');
-$router->get('/accounts/{account}', 'AccountController@show');
-$router->post('/accounts', 'AccountController@store');
-$router->put('/accounts/{accounts}','AccountController@update');
-$router->delete('/accounts/{accounts}','AccountController@destroy');
-
-
-$router->get('/accouts/{account}/contact','ContactController@index');
-$router->post('/accouts/{account}/contact','ContactController@store');
-$router->get('/accouts/{account}/contact/{contact}','ContactController@show');
-$router->put('/accouts/{account}/contact/{contact}','ContactController@update');
-$router->delete('/accouts/{account}/contact/{contact}','ContactController@destroy');
-
-
-
-$router->get('/accouts/{account}/contact/{contact}/opportunity','OpportunityController@index');
-$router->post('/accouts/{account}/contact/{contact}/opportunity','OpportunityController@store');
-$router->get('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@show');
-$router->put('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@update');
-$router->delete('/accouts/{account}/contact/{contact}/opportunity/{opportunity}','OpportunityController@destroy');
-
-
-$router->get('/meetings', 'MeetingController@index');
-$router->get('/meetings/{account}', 'MeetingController@show');
-$router->post('/meetings', 'MeetingController@store');
-$router->put('/meetings/{meetings}','MeetingController@update');
-$router->delete('/meetings/{meetings}','MeetingController@destroy');
-
-
-$router->get('/messages', 'MessageController@index');
-$router->get('/messages/{message}', 'MessageController@show');
-$router->post('/messages', 'MessageController@store');
-$router->put('/messages/{messages}','MessageController@update');
-$router->delete('/messages/{messages}','MessageController@destroy');
