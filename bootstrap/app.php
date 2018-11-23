9cb5eaf6f955eb014915f20efc8d67d1ebe45668
Blankphrase <ckasera6486@gmail.com>
2018-11-23 11:59:28 +0300

    register event service

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -86,9 +86,9 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 
 $app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 /*
