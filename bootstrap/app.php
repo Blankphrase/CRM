cc89c1cccc96e2d07ca40eeef0a24ae90c766ec0
Blankphrase <ckasera6486@gmail.com>
2018-11-23 19:13:28 +0300

    register mail and notifications

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,6 +35,8 @@ if (!class_exists('DingoRoute')) {
 
 $app->withEloquent();
 
+$app->configure('mail');
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
@@ -56,6 +58,12 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
+
+$app->register(Illuminate\Notifications\NotificationServiceProvider::class);
+$app->withFacades(true, [
+ ‘Illuminate\Support\Facades\Notification’ => ‘Notification’,
+]);
+$app->alias(‘mailer’, \Illuminate\Contracts\Mail\Mailer::class);
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
@@ -86,10 +94,11 @@ $app->routeMiddleware([
 |
 */
 
+$app->register(\Illuminate\Mail\MailServiceProvider::class);
+$app->register(PhanAn\CascadingConfig\CascadingConfigServiceProvider::class);
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
-
 $app->register(Dingo\Api\Provider\LumenServiceProvider::class);
 /*
 
