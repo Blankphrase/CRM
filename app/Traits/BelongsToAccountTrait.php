b8ac0ffe2c1ad3d1afe06a550217b8892a15ec72
Blankphrase <ckasera6486@gmail.com>
2018-11-24 13:07:33 +0300

    trait to handle account relationship to the other models

--- app/Traits/BelongsToAccountTrait.php
+++ app/Traits/BelongsToAccountTrait.php
@@ -11,4 +11,23 @@ trait BelongsToAccountTrait {
         return $this->belongsTo(Account::class);
     }
 
+    /**
+     * @param Account $account
+     * @return $this
+     */
+    public function setAccount(Account $account)
+    {
+        $this->account()->associate($account);
+
+        return $this;
+    }
+
+    /**
+     * @return Account|null;
+     */
+    public function getAccount()
+    {
+        return $this->getAttribute('account');
+    }
+
 }
