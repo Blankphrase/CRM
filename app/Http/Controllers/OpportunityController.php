adbf39e2317a5283bb9f883b60987d7681c54f83
Blankphrase <ckasera6486@gmail.com>
2018-11-25 22:38:44 +0300

    create opportunity controller

--- app/Http/Controllers/OpportunityController.php
+++ app/Http/Controllers/OpportunityController.php
@@ -1,8 +1,7 @@
-?php
+<?php
 
 namespace App\Http\Controllers;
 
-use App\Account;
 use App\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,56 +17,42 @@ class OpportunityController extends Controller
     {
         $this->middleware('auth');
     }
-    public function index($accountId,$contactId)
+    public function index($contactId)
     {
-        $account=Account::find($accountId);
-          if (Auth::user()->id !== $account->user_id) {
-            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
-        }
-        $contact=$account->contacts()->find($contactId);
+        
+        $contact=contacts()->find($contactId);
+
         return response()->json(['opportunities'=>$contact->opportunities]);
     }
-    public function show($accountId,$contactId,$opportunityId)
+    public function show($contactId,$opportunityId)
     {
-        $account=Account::find($accountId);
-          if (Auth::user()->id !== $account->user_id) {
-            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
-        }
-        $contact = $account->contacts()->find($contactId);
+    
+        $contact = contacts()->find($contactId);
         $opportunity=$contact->opportunities()->find($opportunityId);
         return response()->json(['status'=>'success','opportunity'=>$opportunity]);
     }
-    public function store(Request $request, $accountId,$contactId)
+    public function store(Request $request, $contactId)
     {
         $this->validate($request,['name'=>'required']);
-        $account=account::find($accountId);
-        if (Auth::user()->id !== $account->user_id) {
-            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
-        }
-        $account->contacts()->find($contactId)->opportunities()->create([
+        
+        $contact->contacts()->find($contactId)->opportunities()->create([
             'name'    => $request->name,
         ]);
         return response()->json(['message' => 'success'], 200);
     }
-    public function update(Request $request, $accountId,$contactId,$opportunityId)
+    public function update(Request $request, $contactId,$opportunityId)
     {
         $this->validate($request,['name'=>'required']);
-        $account = account::find($accountId);
-        if (Auth::user()->id !== $account->user_id) {
-            return response()->json(['status' => 'error', 'message' => 'unauthorized'], 401);
-        }
         
-        $opportunity=$account->contacts()->find($contactId)->opportunities()->find($opportunityId);
+        $opportunity=$contact->contacts()->find($contactId)->opportunities()->find($opportunityId);
         $opportunity->update($request->all());
         return response()->json(['message' => 'success', 'opportunity' => $opportunity], 200);
     }
-    public function destroy($accountId,$contactId,$opportunityId)
+    public function destroy($contactId,$opportunityId)
     {
-        $account=account::find($accountId);
-        if(Auth::user()->id !== $account->user_id) {
-            return response()->json(['status'=>'error','message'=>'unauthorized'],401);
-        }
-        $opportunity=$account->contacts()->find($contactId)->opportunities()->find($opportunityId);
+        $contact=contact::find($contactId);
+        
+        $opportunity=$contact->contacts()->find($contactId)->opportunities()->find($opportunityId);
         if ($opportunity->delete()) {
             return response()->json(['status' => 'success', 'message' => 'opportunity Deleted Successfully']);
         }
